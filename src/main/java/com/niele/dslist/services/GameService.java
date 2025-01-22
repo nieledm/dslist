81f8e52f6e79a7894b5a8210bab6f6fda8682b1f
nieledm <nieledm@gmail.com>
2025-01-22 13:07:23 -0300

    GET game by id

--- src/main/java/com/niele/dslist/services/GameService.java
+++ src/main/java/com/niele/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.niele.dslist.dto.GameDTO;
 import com.niele.dslist.dto.GameMinDTO;
 import com.niele.dslist.entities.Game;
 import com.niele.dslist.repositories.GameRepository;
@@ -16,11 +18,21 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	// Anotation para o método se torne "transacional" (ASID)
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	
 	/*public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(GameMinDTO::new).toList();
 	}*/
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
