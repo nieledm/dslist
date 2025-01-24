ca05cc60531d2aa8d24cb82e77ff319ca0fb2bdc
nieledm <nieledm@gmail.com>
2025-01-24 18:48:55 -0300

    Move games on list

--- src/main/java/com/niele/dslist/services/GameListService.java
+++ src/main/java/com/niele/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.niele.dslist.dto.GameListDTO;
 import com.niele.dslist.entities.GameList;
+import com.niele.dslist.projections.GameMinProjection;
 import com.niele.dslist.repositories.GameListRepository;
+import com.niele.dslist.repositories.GameRepository;
 
 //Injetando dependencias de componentes no sistema
 @Service
@@ -17,11 +19,34 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();		
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);	
+		
+		//Removendo e add o obj da posição atual para a nova posição
+		GameMinProjection obj = list.remove(sourceIndex);	
+		list.add(destinationIndex, obj);
+		
+		//Variáveis para alterar a lista ou do inicio até o destino ou do destino até o inicio
+		//Isso vai depender da diferença dos index inicial e final
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		//Atualizando posição na lista
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 
 }
